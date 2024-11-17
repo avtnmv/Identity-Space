852400a0f709901ab445a7bda3fde5de3b215e01
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-11-17 23:40:30 +0200

    fix: minor fixes and improvements

--- burger-menu.js
+++ burger-menu.js
@@ -6,9 +6,7 @@ const toggleMenu = () => {
     const show = !burgerMenu.classList.contains('show');
     burgerMenu.classList.toggle('show', show);
     burgerIcon.classList.toggle('active', show);
-    document.body.classList.toggle('no-scroll', show);
 };
 
 burgerIcon.addEventListener('click', toggleMenu);
-
 burgerLinks.forEach(link => link.addEventListener('click', () => toggleMenu(false)));
